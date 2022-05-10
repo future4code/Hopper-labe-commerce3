a2844615bdfc5e98fb6980f92cc79e4f92ef1171
Daniel Matos <danielsegatto5@gmail.com>
2022-05-10 19:00:36 -0300

    Estrutura inicial

---
+++ src/components/Carrinho/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default class Carrinho extends React.Component {
+  render() {
+    return <Container>Carrinho</Container>;
+  }
+}
