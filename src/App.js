c40750f4ce286d6b277ff764075a711bcc5b07b9
Daniel Matos <danielsegatto5@gmail.com>
2022-05-11 22:12:04 -0300

    Criei o componente Filtros e apliquei ele no Produtos

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ import Filtros from "./components/Filtros";
 import Produtos from "./components/Produtos";
 import Carrinho from "./components/Carrinho";
 
+import jproducts from "./data/products.json";
+
 class App extends React.Component {
+  state = {
+    products: jproducts,
+    filters: {
+      minPrice: "",
+      maxPrice: "",
+      name: "",
+    },
+  };
+
+  updateMinPrice = (ev) => {
+    this.setState({
+      filters: { ...this.state.filters, minPrice: ev.target.value },
+    });
+  };
+  updateMaxPrice = (ev) => {
+    this.setState({
+      filters: { ...this.state.filters, maxPrice: ev.target.value },
+    });
+  };
+  updateName = (ev) => {
+    this.setState({
+      filters: { ...this.state.filters, name: ev.target.value },
+    });
+  };
+
   render() {
     return (
       <div>
-        <Filtros />
-        <Produtos />
+        <Filtros
+          filters={this.state.filters}
+          updateMinPrice={this.updateMinPrice}
+          updateMaxPrice={this.updateMaxPrice}
+          updateName={this.updateName}
+        />
+        <Produtos products={this.state.products} filters={this.state.filters} />
         <Carrinho />
       </div>
     );
